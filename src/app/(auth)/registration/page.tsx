b03423d9c5b2bdb992562b8081bf244614c4d9c7
suntao99 <lphan@taoshi.io>
2024-04-25 00:46:00 -0700

    chore(logs): removed them

--- src/app/(auth)/registration/page.tsx
+++ src/app/(auth)/registration/page.tsx
@@ -40,8 +40,7 @@ export default async function Page() {
 
   const subscriptions = await getSubscriptions({ userId: user.id });
 
-
-  const fetchAllValidatorInfo = async (//"5ELCYyfrr8jsHpDqTW5qFcdSfNeSvmLBh6cexfak94dNroHK"
+  const fetchAllValidatorInfo = async (
     validatorArr: ValidatorType[],
     subnets: SubnetType[]
   ) => {
@@ -58,12 +57,14 @@ export default async function Page() {
                   "No netUid found for subnet ID:",
                   endpoint.subnet
                 );
-                return endpoint; // Return the endpoint unchanged if no netUid is found
+                return endpoint;
               }
               try {
-                console.log(netUid, validator.hotkey)
-                const stats = await fetchValidatorInfo(netUid, validator.hotkey);
-               // const info = await fetchValidatorInfo(1, "5ELCYyfrr8jsHpDqTW5qFcdSfNeSvmLBh6cexfak94dNroHK");
+                console.log(netUid, validator.hotkey);
+                const stats = await fetchValidatorInfo(
+                  netUid,
+                  validator.hotkey
+                );
 
                 return { ...endpoint, stats };
               } catch (error) {
@@ -73,14 +74,13 @@ export default async function Page() {
                   "with error:",
                   error
                 );
-                return { ...endpoint, error: error?.message }; // Include error info in the endpoint data
+                return { ...endpoint, error: error?.message };
               }
             })
           );
           return { ...validator, endpoints: endpointsWithInfo };
         })
       );
-
 
       return validatorWithInfo;
     } catch (error) {
@@ -90,25 +90,6 @@ export default async function Page() {
   };
 
   const validatorWithInfo = await fetchAllValidatorInfo(validatorArr, subnets);
-
-
-  // if (registrationData?.subnet?.endpoints) {
-  //   const fetchData = async () => {
-  //     const validatorsWithInfo = await Promise.all(
-  //       registrationData.subnet.endpoints.flatMap(
-  //         async (endpoint: EndpointType) => {
-  //           const validator = endpoint?.validators;
-  //           console.log("registrationData.subnet.netUid, validator.hotkey", registrationData.subnet.netUid, validator.hotkey)
-  //           const info = await fetchValidatorInfo(1, "5ELCYyfrr8jsHpDqTW5qFcdSfNeSvmLBh6cexfak94dNroHK");
-  //           return { ...validator, info };
-  //           //return validator;
-  //         }
-  //       )
-  //     );
-  //     console.log("validator:::::", validatorsWithInfo);
-
-  //     setFilteredValidators(validatorsWithInfo);
-  //   };
 
   return (
     <Registration
