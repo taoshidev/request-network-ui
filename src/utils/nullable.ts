d214a4867b817ac039571c87e790387ed263225b
suntao99 <lphan@taoshi.io>
2024-04-29 18:23:34 -0700

    fix(lint): fix linting error

--- src/utils/nullable.ts
+++ src/utils/nullable.ts
@@ -6,7 +6,7 @@ export function nullableSchema<T extends z.ZodRawShape>(
   const newShape: any = {};
   Object.keys(schema.shape).forEach((key) => {
     const originalField = schema.shape[key as keyof T];
-    newShape[key] = originalField.nullable();
+    newShape[key] = originalField?.nullable();
   });
   return z.object(newShape) as z.ZodObject<{
     [K in keyof T]: z.ZodNullable<T[K]>;
