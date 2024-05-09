4ca5937e095857eb98ae7fe558f5f8a6451f7afa
suntao99 <lphan@taoshi.io>
2024-05-09 12:38:54 -0700

    fix(lint): fix ts lint

--- src/components/SubnetValidator/SubnetValidator.tsx
+++ src/components/SubnetValidator/SubnetValidator.tsx
@@ -61,91 +61,93 @@ export function SubnetValidator({
         Choose a Validator for {registrationData?.subnet?.label} Subnet
       </Title>
       <Grid>
-        {(validators || [])?.map((validator: ValidatorType) => (
-          <Grid.Col key={validator.id} span={3}>
-            <Card>
-              <Text className="font-bold mb-4" truncate>
-                {validator?.name || "Validator"}
-              </Text>
-              <Group className="justify-between items-center mb-2">
-                <Text className="text-sm">Active:</Text>
-                <Badge size="sm" variant="light">
-                  {validator?.neuronInfo?.active!.toString() || "FALSE"}
-                </Badge>
-              </Group>
-              <Group className="justify-between items-center mb-2">
-                <Text className="text-sm">Updated:</Text>
-                <Badge size="sm" variant="light">
-                  {dayjs(validator?.neuronInfo?.last_updated).format(
-                    "MMM DD, YYYY"
-                  ) || "-"}
-                </Badge>
-              </Group>
-              <Group className="justify-between items-center mb-2">
-                <Text className="text-sm">V-Trust:</Text>
-                <Badge size="sm" variant="light">
-                  {validator?.neuronInfo?.validator_trust!.toString() || "-"}
-                </Badge>
-              </Group>
-              <Group className="justify-between items-center mb-2">
-                <Text className="text-sm">Trust:</Text>
-                <Badge size="sm" variant="light">
-                  {validator?.neuronInfo?.trust!.toString() || "-"}
-                </Badge>
-              </Group>
-              <Group className="justify-between items-center mb-2">
-                <Text className="text-sm">Endpoints:</Text>
-                <Badge size="sm" variant="light">
-                  {validator?.endpoints?.length}
-                </Badge>
-              </Group>
-              <Group className="justify-between items-center mb-2">
-                <Text className="text-sm">Consensus:</Text>
-                <Badge size="sm" variant="light">
-                  {validator?.neuronInfo?.consensus!.toString() || "-"}
-                </Badge>
-              </Group>
-              <Group className="justify-between items-center mb-2">
-                <Text className="text-sm">Dividends:</Text>
-                <Badge size="sm" variant="light">
-                  {validator?.neuronInfo?.dividends!.toString() || "-"}
-                </Badge>
-              </Group>
-              <Group className="justify-between items-center mb-2">
-                <Text className="text-sm">Emission:</Text>
-                <Badge size="sm" variant="light">
-                  {validator?.neuronInfo?.emission!.toString() || "-"}
-                </Badge>
-              </Group>
-              <Group className="justify-between items-center mb-2">
-                <Text className="text-sm">Incentive:</Text>
-                <Badge size="sm" variant="light">
-                  {validator?.neuronInfo?.incentive!.toString() || "-"}
-                </Badge>
-              </Group>
-              <Group className="justify-between items-center mb-6">
-                <Text className="text-sm">Rank:</Text>
-                <Badge size="sm" variant="light">
-                  {validator?.neuronInfo?.rank!.toString() || "-"}
-                </Badge>
-              </Group>
+        {(validators || [])?.map?.(
+          (validator: ValidatorType & { neuronInfo: any }) => (
+            <Grid.Col key={validator.id} span={3}>
+              <Card>
+                <Text className="font-bold mb-4" truncate>
+                  {validator?.name || "Validator"}
+                </Text>
+                <Group className="justify-between items-center mb-2">
+                  <Text className="text-sm">Active:</Text>
+                  <Badge size="sm" variant="light">
+                    {validator?.neuronInfo?.active!.toString() || "FALSE"}
+                  </Badge>
+                </Group>
+                <Group className="justify-between items-center mb-2">
+                  <Text className="text-sm">Updated:</Text>
+                  <Badge size="sm" variant="light">
+                    {dayjs(validator?.neuronInfo?.last_updated).format(
+                      "MMM DD, YYYY"
+                    ) || "-"}
+                  </Badge>
+                </Group>
+                <Group className="justify-between items-center mb-2">
+                  <Text className="text-sm">V-Trust:</Text>
+                  <Badge size="sm" variant="light">
+                    {validator?.neuronInfo?.validator_trust!.toString() || "-"}
+                  </Badge>
+                </Group>
+                <Group className="justify-between items-center mb-2">
+                  <Text className="text-sm">Trust:</Text>
+                  <Badge size="sm" variant="light">
+                    {validator?.neuronInfo?.trust!.toString() || "-"}
+                  </Badge>
+                </Group>
+                <Group className="justify-between items-center mb-2">
+                  <Text className="text-sm">Endpoints:</Text>
+                  <Badge size="sm" variant="light">
+                    {validator?.endpoints?.length}
+                  </Badge>
+                </Group>
+                <Group className="justify-between items-center mb-2">
+                  <Text className="text-sm">Consensus:</Text>
+                  <Badge size="sm" variant="light">
+                    {validator?.neuronInfo?.consensus!.toString() || "-"}
+                  </Badge>
+                </Group>
+                <Group className="justify-between items-center mb-2">
+                  <Text className="text-sm">Dividends:</Text>
+                  <Badge size="sm" variant="light">
+                    {validator?.neuronInfo?.dividends!.toString() || "-"}
+                  </Badge>
+                </Group>
+                <Group className="justify-between items-center mb-2">
+                  <Text className="text-sm">Emission:</Text>
+                  <Badge size="sm" variant="light">
+                    {validator?.neuronInfo?.emission!.toString() || "-"}
+                  </Badge>
+                </Group>
+                <Group className="justify-between items-center mb-2">
+                  <Text className="text-sm">Incentive:</Text>
+                  <Badge size="sm" variant="light">
+                    {validator?.neuronInfo?.incentive!.toString() || "-"}
+                  </Badge>
+                </Group>
+                <Group className="justify-between items-center mb-6">
+                  <Text className="text-sm">Rank:</Text>
+                  <Badge size="sm" variant="light">
+                    {validator?.neuronInfo?.rank!.toString() || "-"}
+                  </Badge>
+                </Group>
 
-              <Button
-                disabled={!validator.verified}
-                variant={
-                  registrationData &&
-                  registrationData?.validator?.id === validator.id
-                    ? ""
-                    : "outline"
-                }
-                fullWidth
-                onClick={() => handleSubscribeClick(validator)}
-              >
-                {buttonText(validator)}
-              </Button>
-            </Card>
-          </Grid.Col>
-        ))}
+                <Button
+                  disabled={!validator.verified}
+                  variant={
+                    registrationData &&
+                    registrationData?.validator?.id === validator.id
+                      ? ""
+                      : "outline"
+                  }
+                  fullWidth
+                  onClick={() => handleSubscribeClick(validator)}
+                >
+                  {buttonText(validator)}
+                </Button>
+              </Card>
+            </Grid.Col>
+          )
+        )}
       </Grid>
     </Box>
   );
