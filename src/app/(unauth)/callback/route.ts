ec16587d7b5fb2f2e6e65f547cf8a348f007bdd7
suntao99 <lphan@taoshi.io>
2024-05-09 11:54:58 -0700

    fix(google): added oauth

--- src/app/(unauth)/callback/route.ts
+++ src/app/(unauth)/callback/route.ts
@@ -1,33 +1,44 @@
-import { createServerClient } from "@supabase/ssr";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const cookieStore = cookies();
-
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
-    {
-      cookies: {
-        get(name) {
-          return cookieStore.get(name)?.value;
-        },
-        set(name, value, options) {
-          cookieStore.set({ name, value, ...options });
-        },
-        remove(name, options) {
-          cookieStore.set({ name, value: "", ...options });
-        },
-      },
-    }
-  );
-  const { searchParams } = new URL(request.url);
+  const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
-
+  // if "next" is in param, use it as the redirect URL
+  const next = searchParams.get("next") ?? "/";
   if (code) {
-    await supabase.auth.exchangeCodeForSession(code);
+    const cookieStore = cookies();
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          get(name: string) {
+            return cookieStore.get(name)?.value;
+          },
+          set(name: string, value: string, options: CookieOptions) {
+            cookieStore.set({ name, value, ...options });
+          },
+          remove(name: string, options: CookieOptions) {
+            cookieStore.delete({ name, ...options });
+          },
+        },
+      }
+    );
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+
+    if (!error) {
+      if (next && next.length > 1) {
+        return NextResponse.redirect(`${origin}${next}`);
+      }
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
   }
 
-  return NextResponse.redirect(new URL("/dashboard", request.url));
+  // return the user to an error page with instructions.
+  // TODO: create this page under src/auth/oauth-error.tsx
+  return NextResponse.redirect(
+    `${process.env.NEXT_PUBLIC_DOMAIN}/auth/oauth-error`
+  );
 }
