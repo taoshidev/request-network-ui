9347049623ef5f44b0454dc8a774cab624303f35
talperin-tao <talperin@taoshi.io>
2024-05-10 10:48:12 -0700

    feat(ProfilePage): add read only content

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,27 +1,53 @@
 "use client";
 
-import { useState } from "react";
 import {
   Container,
-  Center,
   Box,
+  Image,
   Title,
   Text,
-  Stack,
-  PasswordInput,
-  Button,
   TextInput,
+  Grid,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import { useForm } from "@mantine/form";
+import { zodResolver } from "mantine-form-zod-resolver";
+import { UserSchema } from "@/db/types/user";
 
 export function Profile({ user }: any) {
   const [visible, { toggle }] = useDisclosure(false);
+  const form = useForm<Partial<any>>({
+    initialValues: {},
+    validate: zodResolver(UserSchema),
+  });
+
+  function getTitle(title) {
+    return (
+      (title?.[0] || "").toUpperCase() + (title || "").slice(1).toLowerCase()
+    );
+  }
 
   return (
     <Container h="100%">
       <Box mb="xl">
-        <Text size="sm">Account Email</Text>
-        <Title>{user.email}</Title>
+        <Grid grow>
+          <Grid.Col span={6} order={{ base: 2, xs: 1 }}>
+            <Text size="sm">Account Email</Text>
+            <Title>{user.email}</Title>
+          </Grid.Col>
+          <Grid.Col span={3} order={{ base: 1, xs: 2 }}>
+            {user.user_metadata?.avatar_url && (
+              <Image
+                src={user.user_metadata.avatar_url}
+                radius="md"
+                h={75}
+                w={75}
+                fit="cover"
+                className="float-right"
+              />
+            )}
+          </Grid.Col>
+        </Grid>
       </Box>
 
       <Box mb="xl">
@@ -30,25 +56,36 @@ export function Profile({ user }: any) {
           label="Email Address"
           placeholder="Email Address"
           defaultValue={user.email}
+          readOnly
         />
-      </Box>
-
-      <Box mb="xl">
-        <Title order={2}>Password</Title>
-        <Stack>
-          <PasswordInput
-            label="Password"
-            defaultValue="secret"
-            visible={visible}
-            onVisibilityChange={toggle}
+        {user.user_metadata?.name && (
+          <TextInput
+            label="Name"
+            placeholder=""
+            defaultValue={user.user_metadata.name}
+            readOnly
           />
-          <PasswordInput
-            label="Confirm password"
-            defaultValue="secret"
-            visible={visible}
-            onVisibilityChange={toggle}
+        )}
+        {user.user_metadata?.user_name && (
+          <TextInput
+            label="User Name"
+            placeholder=""
+            defaultValue={user.user_metadata.user_name}
+            readOnly
           />
-        </Stack>
+        )}
+        <TextInput
+          label="Logged in With"
+          placeholder="Role"
+          defaultValue={getTitle(user.identities?.[0]?.provider)}
+          readOnly
+        />
+        <TextInput
+          label="Role"
+          placeholder="Role"
+          defaultValue={getTitle(user.user_metadata.role)}
+          readOnly
+        />
       </Box>
 
       <Box mb="xl">
