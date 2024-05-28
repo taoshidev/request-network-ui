710235e81bbfab223cb1f5a718dfd266753babe7
talperin-tao <talperin@taoshi.io>
2024-05-28 14:58:44 -0700

    fix(cypress): suppress hydration errors to allow cypress to run

---
+++ cypress/e2e/consumer-registration-happy-path.cy.ts
@@ -0,0 +1,8 @@
+describe('Happy Path Exploration', () => {
+  beforeEach(() => {
+    cy.session('verify-key', () => {
+    });
+  });
+  it('should navigate to first step', () => {
+  });
+});
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -18,3 +18,18 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.on("uncaught:exception", (err) => {
+  // Cypress and React Hydrating the document don't get along
+  // for some unknown reason. Hopefully, we figure out why eventually
+  // so we can remove this.
+  // https://github.com/remix-run/remix/issues/4822#issuecomment-1679195650
+  // https://github.com/cypress-io/cypress/issues/27204
+  if (
+    /hydrat/i.test(err.message) ||
+    /Minified React error #418/.test(err.message) ||
+    /Minified React error #423/.test(err.message)
+  ) {
+    return false
+  }
+});
