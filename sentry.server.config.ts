d90cd75c4e23cdb7a60f0ce39de0a766d48f97d1
talperin-tao <talperin@taoshi.io>
2024-05-29 15:04:38 -0700

    fix(workflows): disable sentry for ci

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -4,17 +4,19 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  enabled: ['production', 'staging'].includes(process.env.NEXT_PUBLIC_NODE_ENV as string),
-
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-
-  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: process.env.NODE_ENV === 'development',
+if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    enabled: ['production', 'staging'].includes(process.env.NEXT_PUBLIC_NODE_ENV as string),
   
-});
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
+  
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+  
+    // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+    // spotlight: process.env.NODE_ENV === 'development',
+    
+  });
+}
