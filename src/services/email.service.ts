06b637fbdaaa73815d8817e00c34bfebd492a9de
talperin-tao <talperin@taoshi.io>
2024-05-31 10:21:34 -0700

    refactor(email): use const for user and pass

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -44,10 +44,7 @@ export default class EmailService {
         host: "smtp.gmail.com",
         port: 465,
         secure: true,
-        auth: {
-          user: process.env.EMAIL_FROM,
-          pass: process.env.EMAIL_PASS,
-        },
+        auth: { user, pass },
       });
     }
   }
