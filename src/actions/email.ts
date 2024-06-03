4f9cf20730ca8fa14ae79383ebcfe27118dd1ea2
talperin-tao <talperin@taoshi.io>
2024-06-03 00:26:05 -0700

    fix(email): temporarily disable email graphic

--- src/actions/email.ts
+++ src/actions/email.ts
@@ -7,17 +7,17 @@ import { randomBytes } from "crypto";
 const emailService = new EmailService();
 
 export const sendEmail = async (mailerConfig: IEmailOptions) => {
-  const attachments = [
-    {
-      filename: "request-network.png",
-      path: `${process.cwd()}/public/images/request-network.png`,
-      cid: `${randomBytes(10).toString("hex")}-request-network.png`, //same cid value as in the html img src
-    },
-  ];
+  // const attachments = [
+  //   {
+  //     filename: "request-network.png",
+  //     path: `${process.cwd()}/public/images/request-network.png`,
+  //     cid: `${randomBytes(10).toString("hex")}-request-network.png`, //same cid value as in the html img src
+  //   },
+  // ];
 
   if (!mailerConfig.attachments) {
-    mailerConfig.attachments = attachments;
-    mailerConfig.templateVariables.attachments = attachments;
+    // mailerConfig.attachments = attachments;
+    // mailerConfig.templateVariables.attachments = attachments;
   }
 
   return emailService.send(mailerConfig);
