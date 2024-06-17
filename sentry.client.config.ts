b34f68a2ede79c55c8be01ff986763cca82405e6
talperin-tao <169073418+talperin-tao@users.noreply.github.com>
2024-06-17 16:11:19 -0700

    feat(Sentry): add context to sentry errors and enable modal for staging (#178)
    * feat(Sentry): add user data to error messages
    
    * feat(auth-provider): set sentry context

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -7,28 +7,35 @@ import * as Sentry from "@sentry/nextjs";
 if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
   Sentry.init({
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-    environment:process.env.NEXT_PUBLIC_NODE_ENV as string,
-    // beforeSend(event, hint) {
-    //   // Check if it is an exception, and if so, show the report dialog
-    //   if (event.exception && event.event_id && process.env.NEXT_PUBLIC_NODE_ENV !== 'development') {
-    //     Sentry.showReportDialog({ eventId: event.event_id });
-    //   }
-    //   return event;
-    // },
-    enabled: ['production', 'staging'].includes(process.env.NEXT_PUBLIC_NODE_ENV as string),
-  
+    environment: process.env.NEXT_PUBLIC_NODE_ENV as string,
+    beforeSend(event, hint) {
+      // Check if it is an exception, and if so, show the report dialog
+      // Only open dialog on test environment
+      if (
+        event.exception &&
+        event.event_id &&
+        process.env.NEXT_PUBLIC_NODE_ENV === "staging"
+      ) {
+        Sentry.showReportDialog({ eventId: event.event_id });
+      }
+      return event;
+    },
+    enabled: ["production", "staging"].includes(
+      process.env.NEXT_PUBLIC_NODE_ENV as string
+    ),
+
     // Adjust this value in production, or use tracesSampler for greater control
     tracesSampleRate: 1,
-  
+
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: false,
-  
+
     replaysOnErrorSampleRate: 1.0,
-  
+
     // This sets the sample rate to be 10%. You may want this to be 100% while
     // in development and sample at a lower rate in production
     replaysSessionSampleRate: 0.1,
-  
+
     // You can remove this option if you're not planning to use the Sentry Session Replay feature:
     integrations: [
       Sentry.feedbackIntegration({
