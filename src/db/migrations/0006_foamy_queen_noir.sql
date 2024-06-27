4363d0e99555bb0df8020b44adcd5ed6ff0596d3
talperin-tao <talperin@taoshi.io>
2024-06-27 11:46:06 -0700

    fix(migrations): drop last migration

--- src/db/migrations/0006_foamy_queen_noir.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "auth"."users" ADD COLUMN "paypal_enabled" boolean DEFAULT true;--> statement-breakpoint
-ALTER TABLE "validators" ADD COLUMN "paypal_enabled" boolean DEFAULT false;--> statement-breakpoint
-ALTER TABLE "validators" ADD COLUMN "paypal_live_mode" boolean DEFAULT false;
