71330fced516c07a07d96def1d3ad9ed1d81a135
suntao99 <lphan@taoshi.io>
2024-06-26 18:40:47 -0700

    refactor(code): move header footer to unauth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,13 +1,11 @@
-import { Suspense } from 'react'
 import ClientLayout from "./client-layout";
 import NextTopLoader from "nextjs-toploader";
-import Loading from "./loading";
 
 export default async function Layout({ children }: any) {
     return (
-      <Suspense fallback={<Loading />}>
+      <>
         <NextTopLoader color="#D36737" showSpinner={false} shadow={false} />
         <ClientLayout>{children}</ClientLayout>
-      </Suspense>
+      </>
     );
 }
