08ed59dea1a058db7059fb151940563ca75d15c7
talperin-tao <talperin@taoshi.io>
2024-07-04 14:45:46 -0700

    feat(validators): fallback ui and add missing ui

--- src/components/ValidatorStatus.tsx
+++ src/components/ValidatorStatus.tsx
@@ -3,9 +3,13 @@
 import { EndpointType } from "@/db/types/endpoint";
 import { UserType } from "@/db/types/user";
 import { ValidatorType } from "@/db/types/validator";
-import { Badge, Box, Card, Group, Table, Text } from "@mantine/core";
+import { Alert, Badge, Box, Card, Group, Table, Text } from "@mantine/core";
 import UptimeFormatter from "./Formatters/UptimeFormatter";
-import { IconAlertTriangle, IconCircleCheck } from "@tabler/icons-react";
+import {
+  IconAlertCircle,
+  IconAlertTriangle,
+  IconCircleCheck,
+} from "@tabler/icons-react";
 import clsx from "clsx";
 import useSWR from "swr";
 import { getValidatorStatusPage } from "@/actions/validators";
@@ -58,167 +62,190 @@ export default function ValidatorStatus({
 
   return (
     <Box>
-      <Group className="flex items-stretch grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-items-stretch">
-        {validators?.map((validator) => (
-          <Card
-            key={validator.id}
-            shadow="sm"
-            padding="lg"
-            withBorder
-            className={clsx(
-              "card-table",
-              validator.health?.message?.toLowerCase() !== "ok" && "opacity-70"
-            )}
+      <Group
+        className={clsx(
+          "flex items-stretch grid grid-cols-1 gap-5 justify-items-stretch",
+          validators?.length && "md:grid-cols-2 lg:grid-cols-3 "
+        )}
+      >
+        {!validators?.length ? (
+          <Alert
+            className="w-full mt-8 shadow-sm"
+            color="orange"
+            icon={<IconAlertCircle />}
           >
-            <Box className="-m-2 -mt-5 pb-[5rem]">
-              <Box
-                className={clsx(
-                  "p-3 -mx-3",
-                  validator.health?.message?.toLowerCase() === "ok"
-                    ? "text-green-800"
-                    : "text-black"
-                )}
-              >
-                {validator.health?.message?.toLowerCase() === "ok" ? (
-                  <IconCircleCheck className="inline-block" />
-                ) : (
-                  <IconAlertTriangle className="inline-block" />
-                )}{" "}
-                {validator.name}{" "}
-                {validator.health?.message?.toLowerCase() === "ok" ? (
-                  <Badge className="float-end bg-green-600 zoom in">
-                    Online
-                  </Badge>
-                ) : (
-                  <Badge className="float-end bg-red-700 zoom in">
-                    Offline
-                  </Badge>
-                )}
-              </Box>
-              <Table className="w-100 table-align-top">
-                <Table.Tbody>
-                  <Table.Tr>
-                    <Table.Th>Subnets</Table.Th>
-                    <Table.Td className="text-right">
-                      {(validator.endpoints || [])
-                        .filter(
-                          (
-                            endpoint: EndpointType,
-                            index: number,
-                            array: EndpointType[]
-                          ) =>
-                            array.findIndex(
-                              (firstEndpoint) =>
-                                firstEndpoint?.subnet?.label ===
-                                endpoint?.subnet?.label
-                            ) === index
-                        )
-                        .map((endpoint: any) => (
-                          <Badge key={endpoint.id} color="grey">
-                            {endpoint?.subnet?.label}
-                          </Badge>
-                        ))}
-                    </Table.Td>
-                  </Table.Tr>
-                  <Table.Tr>
-                    <Table.Th>Base Url</Table.Th>
-                    <Table.Td className="text-right">
-                      {validator?.baseApiUrl}
-                    </Table.Td>
-                  </Table.Tr>
-                  <Table.Tr>
-                    <Table.Th>Hotkey</Table.Th>
-                    <Table.Td className="relative text-right">
-                      <Box className="max-w-[200px] text-ellipsis overflow-hidden float-right">
-                        {validator?.hotkey}
-                      </Box>
-                    </Table.Td>
-                  </Table.Tr>
-                  <Table.Tr>
-                    <Table.Th>Net Uid</Table.Th>
-                    <Table.Td className="text-right">
-                      {validator?.bittensorNetUid || "-"}
-                    </Table.Td>
-                  </Table.Tr>
-                  <Table.Tr>
-                    <Table.Th>Verified</Table.Th>
-                    <Table.Td className="text-right">
-                      {validator?.verified ? "Yes" : "No"}
-                    </Table.Td>
-                  </Table.Tr>
-                  <Table.Tr>
-                    <Table.Td colSpan={2}>
-                      <Box className="flex justify-center">
-                        {validator?.stripeEnabled &&
-                          validator.endpoints?.some((endpoint) =>
+            <Text className="mb-2 text-base">
+              There are no validators available at the moment.{" "}
+              {user?.user_metadata?.role === "validator" ? (
+                <>Create new validators to view them from this page.</>
+              ) : (
+                <>Please check back soon!</>
+              )}
+            </Text>
+          </Alert>
+        ) : (
+          validators?.map((validator) => (
+            <Card
+              key={validator.id}
+              shadow="sm"
+              padding="lg"
+              withBorder
+              className={clsx(
+                "card-table",
+                validator.health?.message?.toLowerCase() !== "ok" &&
+                  "opacity-70"
+              )}
+            >
+              <Box className="-m-2 -mt-5 pb-[5rem]">
+                <Box
+                  className={clsx(
+                    "p-3 -mx-3",
+                    validator.health?.message?.toLowerCase() === "ok"
+                      ? "text-green-800"
+                      : "text-black"
+                  )}
+                >
+                  {validator.health?.message?.toLowerCase() === "ok" ? (
+                    <IconCircleCheck className="inline-block" />
+                  ) : (
+                    <IconAlertTriangle className="inline-block" />
+                  )}{" "}
+                  {validator.name}{" "}
+                  {validator.health?.message?.toLowerCase() === "ok" ? (
+                    <Badge className="float-end bg-green-600 zoom in">
+                      Online
+                    </Badge>
+                  ) : (
+                    <Badge className="float-end bg-red-700 zoom in">
+                      Offline
+                    </Badge>
+                  )}
+                </Box>
+                <Table className="w-100 table-align-top">
+                  <Table.Tbody>
+                    <Table.Tr>
+                      <Table.Th>Subnets</Table.Th>
+                      <Table.Td className="text-right">
+                        {(validator.endpoints || [])
+                          .filter(
+                            (
+                              endpoint: EndpointType,
+                              index: number,
+                              array: EndpointType[]
+                            ) =>
+                              array.findIndex(
+                                (firstEndpoint) =>
+                                  firstEndpoint?.subnet?.label ===
+                                  endpoint?.subnet?.label
+                              ) === index
+                          )
+                          .map((endpoint: any) => (
+                            <Badge key={endpoint.id} color="grey">
+                              {endpoint?.subnet?.label}
+                            </Badge>
+                          ))}
+                      </Table.Td>
+                    </Table.Tr>
+                    <Table.Tr>
+                      <Table.Th>Base Url</Table.Th>
+                      <Table.Td className="text-right">
+                        {validator?.baseApiUrl}
+                      </Table.Td>
+                    </Table.Tr>
+                    <Table.Tr>
+                      <Table.Th>Hotkey</Table.Th>
+                      <Table.Td className="relative text-right">
+                        <Box className="max-w-[200px] text-ellipsis overflow-hidden float-right">
+                          {validator?.hotkey}
+                        </Box>
+                      </Table.Td>
+                    </Table.Tr>
+                    <Table.Tr>
+                      <Table.Th>Net Uid</Table.Th>
+                      <Table.Td className="text-right">
+                        {validator?.bittensorNetUid || "-"}
+                      </Table.Td>
+                    </Table.Tr>
+                    <Table.Tr>
+                      <Table.Th>Verified</Table.Th>
+                      <Table.Td className="text-right">
+                        {validator?.verified ? "Yes" : "No"}
+                      </Table.Td>
+                    </Table.Tr>
+                    <Table.Tr>
+                      <Table.Td colSpan={2}>
+                        <Box className="flex justify-center">
+                          {validator?.stripeEnabled &&
+                            validator.endpoints?.some((endpoint) =>
+                              endpoint?.contract?.services?.some(
+                                (service) => +service.price !== 0
+                              )
+                            ) && (
+                              <Badge className="ml-1" color="darkblue">
+                                Stripe
+                              </Badge>
+                            )}
+                          {validator?.payPalEnabled &&
+                            validator.endpoints?.some((endpoint) =>
+                              endpoint?.contract?.services?.some(
+                                (service) => +service.price !== 0
+                              )
+                            ) && (
+                              <Badge className="ml-1" color="blue">
+                                PayPal
+                              </Badge>
+                            )}
+                          {validator.endpoints?.some((endpoint) =>
                             endpoint?.contract?.services?.some(
-                              (service) => +service.price !== 0
+                              (service) => +service.price === 0
                             )
                           ) && (
-                            <Badge className="ml-1" color="darkblue">
-                              Stripe
+                            <Badge className="ml-1 text-black" color="#ccc">
+                              FREE
                             </Badge>
                           )}
-                        {validator?.payPalEnabled &&
-                          validator.endpoints?.some((endpoint) =>
+                          {validator.endpoints?.some((endpoint) =>
                             endpoint?.contract?.services?.some(
-                              (service) => +service.price !== 0
+                              (service) =>
+                                +service.price !== 0 &&
+                                service.currencyType === "USDC"
                             )
                           ) && (
-                            <Badge className="ml-1" color="blue">
-                              PayPal
+                            <Badge className="ml-1" color="#c49f55">
+                              USDC
+                            </Badge>
+                          )}{" "}
+                          {validator.endpoints?.some((endpoint) =>
+                            endpoint?.contract?.services?.some(
+                              (service) =>
+                                +service.price !== 0 &&
+                                service.currencyType === "USDT"
+                            )
+                          ) && (
+                            <Badge className="ml-1" color="green">
+                              USDT
                             </Badge>
                           )}
-                        {validator.endpoints?.some((endpoint) =>
-                          endpoint?.contract?.services?.some(
-                            (service) => +service.price === 0
-                          )
-                        ) && (
-                          <Badge className="ml-1 text-black" color="#ccc">
-                            FREE
-                          </Badge>
-                        )}
-                        {validator.endpoints?.some((endpoint) =>
-                          endpoint?.contract?.services?.some(
-                            (service) =>
-                              +service.price !== 0 &&
-                              service.currencyType === "USDC"
-                          )
-                        ) && (
-                          <Badge className="ml-1" color="#c49f55">
-                            USDC
-                          </Badge>
-                        )}{" "}
-                        {validator.endpoints?.some((endpoint) =>
-                          endpoint?.contract?.services?.some(
-                            (service) =>
-                              +service.price !== 0 &&
-                              service.currencyType === "USDT"
-                          )
-                        ) && (
-                          <Badge className="ml-1" color="green">
-                            USDT
-                          </Badge>
-                        )}
-                        {(validator.endpoints?.length || 0) === 0 && (
-                          <Badge className="ml-1 text-black " color="#ccc">
-                            No Active Endpoints
-                          </Badge>
-                        )}
-                      </Box>
-                    </Table.Td>
-                  </Table.Tr>
-                </Table.Tbody>
-              </Table>
-              <Box className="absolute w-full bottom-0 left-0 pb-2">
-                <Text className="px-3 py-1 text-sm font-bold text-center border border-top">
-                  Uptime
-                </Text>
-                <UptimeFormatter seconds={validator.health?.uptime} />
+                          {(validator.endpoints?.length || 0) === 0 && (
+                            <Badge className="ml-1 text-black " color="#ccc">
+                              No Active Endpoints
+                            </Badge>
+                          )}
+                        </Box>
+                      </Table.Td>
+                    </Table.Tr>
+                  </Table.Tbody>
+                </Table>
+                <Box className="absolute w-full bottom-0 left-0 pb-2">
+                  <Text className="px-3 py-1 text-sm font-bold text-center border border-top">
+                    Uptime
+                  </Text>
+                  <UptimeFormatter seconds={validator.health?.uptime} />
+                </Box>
               </Box>
-            </Box>
-          </Card>
-        ))}
+            </Card>
+          ))
+        )}
       </Group>
     </Box>
   );
